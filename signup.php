bff9087b907eff886cb05cc293e1b6981b8775c2
Siddhi742 <siddhimukkawar@gmail.com>
2020-08-05 15:19:50 +0530

    Signup

---
+++ signup.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<title>WebChat</title>
+ 	<meta charset="UTF-8">
+ 	<meta name="viewport" content="device-width, initial-scale=1.0">
+ 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+ 	<link rel="stylesheet" type="text/css" href="css/SignIn.css">
+ </head>
+ <body>
+ 	<div class="signin">
+ 		<div class="left-section">
+ 			<div class="form-header">
+ 				<h1>WebChat!</h1>
+ 				<p>Create New Account!</p>
+ 			</div>
+ 			<div class="form-group">
+ 				<form class="" action="" method="post">
+                 <input type="text" placeholder="Username" required>
+		            <input type="tel" placeholder="Mobile" required>
+		            <input type="password" placeholder="Password" required>
+		            <input type="submit" value="Sign Up">
+		        </form>
+ 			    <!-- <?php include("signup_user.php"); ?> -->
+ 			    
+ 			    <div class="account">Already Have an Account? <a href="signin.php">Login</a></div>
+ 			</div>
+ 		</div> 
+	 	<div class="right-section">
+	 		<div class="welcome-text">
+		        <h1><b><i>Welcome Back!</i></b></h1>
+		        <p>Quisque ultricies aliquam pellentesque. Aliquam libero orci, efficitur et maximus eget, porttitor vel ex. Sed imperdiet lacus non ipsum pharetra, non sagittis felis semper.</p>
+	        </div>
+	    </div>
+    </div>
+</body>
+</html>
